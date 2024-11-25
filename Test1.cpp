ec01f1a98c3c29cdb11b3fe1ca6fb0fd83ad2d28
DarraghMurr <C23388251@mytudublin.ie>
2024-11-25 16:10:28 +0000

    Sup Dawg

--- Test1.cpp
+++ Test1.cpp
@@ -1,2 +1,19 @@
 //Programming Project 
 //Golf Score Card
+//Hello
+class Test1
+{
+private:
+    /* data */
+public:
+    Test1(/* args */);
+    ~Test1();
+};
+
+Test1::Test1(/* args */)
+{
+}
+
+Test1::~Test1()
+{
+}
