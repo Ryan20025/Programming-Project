e6ab21ac9f92aacf4b94e5483913ddf16ad090ec
DarraghMurr <C23388251@mytudublin.ie>
2024-12-05 14:30:34 +0000

    Hello

--- Test1.cpp
+++ Test1.cpp
@@ -6,8 +6,13 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Hello world!ytfefg" << endl;
+    cout << "Hello world!ytftfg" << endl;
+    cout << "Hello world!ytfpfg" << endl;
+    cout << "Hello world!ytwffg" << endl;
+    cout << "Hello world!ytfttfg" << endl;
+
+
+
     return 0;
 }
-
-
